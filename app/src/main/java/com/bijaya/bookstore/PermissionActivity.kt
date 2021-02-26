e2895cd664d3c4d555b934e7510124320a35cefc
Bijaya <darsannamaste234@gmail.com>
2021-02-26 22:03:57 +0545

    add buttons on dashboard

--- app/src/main/java/com/bijaya/bookstore/PermissionActivity.kt
+++ app/src/main/java/com/bijaya/bookstore/PermissionActivity.kt
@@ -16,7 +16,7 @@ class PermissionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_permission)
-        // Check for permissions
+        // Checking for permissions
         if (!hasPermission()) {
             requestPermission()
         }
