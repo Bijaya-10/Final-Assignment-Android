dedd86e90c86371636690a1944a1cd7c04381f66
Bijaya <darsannamaste234@gmail.com>
2021-03-10 22:37:24 +0545

    adding grid layout

---
+++ app/src/main/java/com/bijaya/bookstore/grid.kt
@@ -0,0 +1,12 @@
+package com.bijaya.bookstore
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bijaya1.weekfiveassignmentone.R
+
+class grid : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_grid)
+    }
+}
---
+++ app/src/main/java/com/bijaya/bookstore/grid2.kt
@@ -0,0 +1,11 @@
+package com.bijaya.bookstore
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class grid2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_grid2)
+    }
+}
