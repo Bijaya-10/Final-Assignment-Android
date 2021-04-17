3eb57f1b7ae5987fb70a9c5feb1c3987253f25f8
Bijaya <darsannamaste234@gmail.com>
2021-04-17 10:22:01 +0545

    wearos work going on

---
+++ soraew/src/main/java/com/example/soraew/WearOsActivity.kt
@@ -0,0 +1,15 @@
+package com.example.soraew
+
+import android.os.Bundle
+import android.support.wearable.activity.WearableActivity
+
+class WearOsActivity : WearableActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_wear_os)
+
+        // Enables Always-on
+        setAmbientEnabled()
+    }
+}
